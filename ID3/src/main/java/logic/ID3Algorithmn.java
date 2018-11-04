d3f5babcd4180835ba6083c1b842ddcc1bcade8a
Andre Hartmann <andrehartmann1993@hotmail.de>
2018-11-04 22:31:32 +0100

    - added model class Tree

---
+++ ID3/src/main/java/logic/ID3Algorithmn.java
@@ -0,0 +1,6 @@
+package logic;
+
+public class ID3Algorithmn {
+
+
+}
---
+++ ID3/src/main/java/model/Tree.java
@@ -0,0 +1,26 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree<T> {
+    @Getter
+    private Node<T> root;
+
+    public Tree(T rootData) {
+        root = new Node<T>();
+        root.data = rootData;
+        root.children = new ArrayList<Node<T>>();
+        root.parent = null;
+    }
+
+    @Getter
+    @Setter
+    public static class Node<T> {
+        private T data;
+        private Node<T> parent;
+        private List<Node<T>> children;
+    }
+}
